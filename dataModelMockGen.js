2366ae6cd43efdf7da48a9d8a95bdc8a83e82a95
Zeev Jelinek <mushmu@gmail.com>
2015-10-06 08:48:48 +0300

    added mock data file

---
+++ dataModelMockGen.js
@@ -0,0 +1,22 @@
+/*
+//generate in http://www.objgen.com/json
+
+items[0]
+  id n = 12345
+  photo s = http://blablabal
+  location
+    long n = 1.434234
+    lat n = 2.530583
+  inBoxId n = 473748937
+  dateAdded = dateInfo
+  dateChanged = dateInfo
+  purchacePrice n = 51.2
+  purchaceCurrency n = ILS
+  purchaceDate n = 51.2
+  WarrantyLengthMonths n = 12
+  currentValue n = 30
+  notes = bla blabla  
+  quantity = 1
+  private b = true
+
+*/  
